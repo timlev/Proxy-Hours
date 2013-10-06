ee42a434778723d3edfc2f9ddd6301133f625625
Tim <timlev@gmail.com>
2013-10-06 13:42:43 -0500

    Added mega.db

---
+++ Proxy_Hours.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Proxy_Hours.ui'
+#
+# Created: Fri Sep 20 20:16:17 2013
+#      by: PyQt4 UI code generator 4.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(800, 600)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8("../../Dropbox/scans/UIs/ProxyHoursApp/src/report.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.gridLayout = QtGui.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.gridLayout.addWidget(self.label, 0, 1, 1, 1)
+        self.log_lineEdit = QtGui.QLineEdit(self.centralwidget)
+        self.log_lineEdit.setReadOnly(True)
+        self.log_lineEdit.setObjectName(_fromUtf8("log_lineEdit"))
+        self.gridLayout.addWidget(self.log_lineEdit, 2, 2, 1, 2)
+        self.OpenpushButton = QtGui.QPushButton(self.centralwidget)
+        self.OpenpushButton.setObjectName(_fromUtf8("OpenpushButton"))
+        self.gridLayout.addWidget(self.OpenpushButton, 0, 3, 1, 1)
+        self.FilelineEdit = QtGui.QLineEdit(self.centralwidget)
+        self.FilelineEdit.setObjectName(_fromUtf8("FilelineEdit"))
+        self.gridLayout.addWidget(self.FilelineEdit, 0, 2, 1, 1)
+        self.label_2 = QtGui.QLabel(self.centralwidget)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.gridLayout.addWidget(self.label_2, 2, 1, 1, 1)
+        self.label_3 = QtGui.QLabel(self.centralwidget)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.gridLayout.addWidget(self.label_3, 3, 1, 1, 1)
+        self.all_data_lineEdit = QtGui.QLineEdit(self.centralwidget)
+        self.all_data_lineEdit.setReadOnly(True)
+        self.all_data_lineEdit.setObjectName(_fromUtf8("all_data_lineEdit"))
+        self.gridLayout.addWidget(self.all_data_lineEdit, 3, 2, 1, 2)
+        self.label_4 = QtGui.QLabel(self.centralwidget)
+        self.label_4.setObjectName(_fromUtf8("label_4"))
+        self.gridLayout.addWidget(self.label_4, 4, 1, 1, 1)
+        self.time_lineEdit = QtGui.QLineEdit(self.centralwidget)
+        self.time_lineEdit.setReadOnly(True)
+        self.time_lineEdit.setObjectName(_fromUtf8("time_lineEdit"))
+        self.gridLayout.addWidget(self.time_lineEdit, 4, 2, 1, 1)
+        self.tableWidget = QtGui.QTableWidget(self.centralwidget)
+        self.tableWidget.setLineWidth(1)
+        self.tableWidget.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
+        self.tableWidget.setAlternatingRowColors(True)
+        self.tableWidget.setTextElideMode(QtCore.Qt.ElideMiddle)
+        self.tableWidget.setObjectName(_fromUtf8("tableWidget"))
+        self.tableWidget.setColumnCount(3)
+        self.tableWidget.setRowCount(0)
+        item = QtGui.QTableWidgetItem()
+        item.setTextAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter|QtCore.Qt.AlignCenter)
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        item.setFont(font)
+        self.tableWidget.setHorizontalHeaderItem(0, item)
+        item = QtGui.QTableWidgetItem()
+        item.setTextAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter|QtCore.Qt.AlignCenter)
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        item.setFont(font)
+        self.tableWidget.setHorizontalHeaderItem(1, item)
+        item = QtGui.QTableWidgetItem()
+        item.setText(_fromUtf8("Proxy Hours"))
+        item.setTextAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter|QtCore.Qt.AlignCenter)
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        item.setFont(font)
+        self.tableWidget.setHorizontalHeaderItem(2, item)
+        self.tableWidget.horizontalHeader().setDefaultSectionSize(200)
+        self.tableWidget.horizontalHeader().setMinimumSectionSize(200)
+        self.gridLayout.addWidget(self.tableWidget, 1, 1, 1, 3)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menuFile = QtGui.QMenu(self.menubar)
+        self.menuFile.setObjectName(_fromUtf8("menuFile"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionCopy = QtGui.QAction(MainWindow)
+        self.actionCopy.setObjectName(_fromUtf8("actionCopy"))
+        self.actionPaste = QtGui.QAction(MainWindow)
+        self.actionPaste.setObjectName(_fromUtf8("actionPaste"))
+        self.actionOpen = QtGui.QAction(MainWindow)
+        self.actionOpen.setObjectName(_fromUtf8("actionOpen"))
+        self.actionQuit = QtGui.QAction(MainWindow)
+        self.actionQuit.setObjectName(_fromUtf8("actionQuit"))
+        self.menuFile.addAction(self.actionOpen)
+        self.menuFile.addAction(self.actionQuit)
+        self.menubar.addAction(self.menuFile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QObject.connect(self.actionQuit, QtCore.SIGNAL(_fromUtf8("activated()")), MainWindow.close)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "Proxy-Hours", None))
+        self.label.setText(_translate("MainWindow", "PDF File:", None))
+        self.OpenpushButton.setText(_translate("MainWindow", "Open", None))
+        self.label_2.setText(_translate("MainWindow", "Proxy Hour Record Written To:", None))
+        self.label_3.setText(_translate("MainWindow", "All Data Written To:", None))
+        self.label_4.setText(_translate("MainWindow", "Running time:", None))
+        self.tableWidget.setSortingEnabled(True)
+        item = self.tableWidget.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "Username", None))
+        item = self.tableWidget.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "Student Name", None))
+        self.menuFile.setTitle(_translate("MainWindow", "File", None))
+        self.actionCopy.setText(_translate("MainWindow", "Copy", None))
+        self.actionCopy.setShortcut(_translate("MainWindow", "Ctrl+C", None))
+        self.actionPaste.setText(_translate("MainWindow", "Paste", None))
+        self.actionPaste.setShortcut(_translate("MainWindow", "Ctrl+V", None))
+        self.actionOpen.setText(_translate("MainWindow", "Open", None))
+        self.actionOpen.setShortcut(_translate("MainWindow", "Ctrl+O", None))
+        self.actionQuit.setText(_translate("MainWindow", "Quit", None))
+        self.actionQuit.setShortcut(_translate("MainWindow", "Ctrl+Q", None))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = QtGui.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
